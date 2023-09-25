60333642db3c13e0848f9213b655682a498805ce
Puddinnd <eieueo@eieueos-MacBook-Pro.local>
2023-09-25 13:50:57 +0700

    init again - change ethers' version

--- contracts/ExampleCounter.sol
+++ contracts/ExampleCounter.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Lock {
+contract ExampleCounter {
     uint public unlockTime;
     address payable public owner;
 
---
+++ test/ExampleCounter.js
@@ -0,0 +1,51 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("ExampleCounter", function () {
+  
+  async function deploy() {
+    /// Get accounts
+    let [deployer, otherAccount] = await ethers.getSigners();
+
+    /// Deploy testing contracts
+    const Counter = await ethers.getContractFactory("ExampleCounter");
+    const requiredAmount = ethers.utils.parseEther("0.1");
+    const answer = 7;
+    const counter = await Counter.connect(owner).deploy(answer, {value: requiredAmount});
+
+    return { counter, deployer, otherAccount };
+  }
+
+  describe("Deployment", () => {
+    it("Deploy Counter contract", async () => {
+      const { counter, deployer } = await deploy();
+      console.log(`deployer addres  : ${await deployer.getAddress()}`);
+      console.log(`contract owner: ${await counter.owner()}`);
+      expect(await counter.owner() == deployer);
+    });
+  });
+
+  describe("Test Counter", () => {
+    it("Increment", async () => {
+      const { counter, deployer } = await deploy();
+      const old_count = parseInt(await counter.get());
+      await counter.connect(deployer).inc();
+      const new_count = parseInt(await counter.get());
+      console.log(`old_count: ${old_count}`);
+      console.log(`new_count: ${new_count}`);
+      expect(new_count).to.equal(old_count+1);
+    });
+
+    it("Decrement", async () => {
+      const { counter, deployer } = await deploy();
+      await counter.connect(deployer).inc();
+      const old_count = parseInt(await counter.get());
+      await counter.connect(deployer).dec();
+      const new_count = parseInt(await counter.get());
+      console.log(`old_count: ${old_count}`);
+      console.log(`new_count: ${new_count}`);
+      expect(new_count).to.equal(old_count-1);
+    });
+  });
+
+});
