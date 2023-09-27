eb38aed0b66edb1e94bac7556e41d6d68ac9462d
Puddinnd <eieueo@eieueos-MacBook-Pro.local>
2023-09-27 11:28:17 +0700

    change description

--- test/Centralized/CentralizedContract_EOA.js
+++ test/Centralized/CentralizedContract_EOA.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("CentralizedContract", function () {
+describe("CentralizedContract EOA", function () {
   
   async function deploy() {
     /// Get accounts
