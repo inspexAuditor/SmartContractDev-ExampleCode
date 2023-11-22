98ed1e765a99cde401b17e37826b4b7958f97614
Puddinnd <eieueo@eieueos-MacBook-Pro.local>
2023-11-22 16:19:26 +0700

    typo

--- test/PriceOracle/OnChain/NftMarketplace_PriceOracle_OnChain_v2.js
+++ test/PriceOracle/OnChain/NftMarketplace_PriceOracle_OnChain_v2.js
@@ -11,7 +11,7 @@ const PAIR_WETH_RESERVE = ethers.utils.parseEther("1");
 const ATTACKER_TOKEN_INITIAL = ethers.utils.parseEther("100000");
 const ATTACKER_ETH_INITIAL = ethers.utils.parseEther("20");
 
-describe("NftMarketplace PriceOracle v2", function () {
+describe("NftMarketplace On-Chain PriceOracle v2", function () {
   
   async function deploy() {
     /// Get accounts
